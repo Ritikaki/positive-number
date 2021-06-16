600026cc84cd00bbf263ea58ab3e97b4a3960c77
Ritikaki <85887141+Ritikaki@users.noreply.github.com>
2021-06-16 08:33:44 +0530

    Add files via upload

---
+++ positive number.py
@@ -0,0 +1,24 @@
+# Python program to print positive Numbers in a List
+
+# list of numbers
+list1 = [12,-7, 5, 64, -14]
+list2 = [12,14,-95,3]
+list =(list1+list2)
+num = 0
+
+# using while loop
+while (num < len(list)):
+
+    # checking condition
+    if list[num] >= 0:
+        print(list[num], end=" ")
+
+    # increment num
+    num += 1
+
+
+
+
+
+
+
